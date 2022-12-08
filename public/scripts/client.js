368cb8eb7f9e2f02b96c0ba0bdbf36987008c78b
Mostafa-Naghipoor <mostafa.naghipoor@hotmail.com>
2022-12-08 12:00:52 -0800

    add form submission by jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,31 +3,14 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// Fake data taken from initial-tweets.json
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
+
+$(function () {
+    //Escape method to avoid XSS attacks
+    const escape = function (str) {
+      let div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
     }
-  ]
   
   const createTweetElement = function(tweet){
     let tweet = `
@@ -51,15 +34,20 @@ const data = [
                 </div>
             </footer>
         </article>`;
-  
-  
-  
     return tweet;   
-  };
-  
-  const $tweet = createTweetElement(tweet);
-  $('#tweets').append($tweet);
-  
+  }
+  const loadTweets = function () {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET'
+    })
+      .done((tweets) => {
+        renderTweets(tweets)
+      })
+      .fail(() => console.log('An error has occurred'))
+      .always(() => console.log('Succesful request'));
+  }
+
   const renderTweets = function (tweets) {
     $('#tweets').empty();
   
@@ -69,4 +57,37 @@ const data = [
       $('#tweets').prepend(newTweet);
     }
   }
-  renderTweets(data);
+    //tweet creation method
+    $('#new-tweet-form').on('submit', function (event) {
+        event.preventDefault();
+    
+        const $tweetText = $(this).children('#tweet-text');
+    
+        if ($tweetText.val().length > 140) {
+          $('.error').html(`<p>There is a limit for written characters (140), Please reduce the text </p>`);
+          $('.error').slideDown('slow');
+          return;
+        } else if ($tweetText.val().length === 0) {
+          $('.error').html(`<p>Please write something to tweet :D</p>`);
+          $('.error').slideDown('slow');
+          return
+        }
+    
+        const formContent = $(this).serialize();
+    
+        $.ajax({
+          url: '/tweets',
+          method: 'POST',
+          data: formContent
+        })
+          .done(() => loadTweets())
+          .fail(() => console.log("Something went wrong"))
+          .always(() => console.log("SUCCESS BAYBEE"));
+    
+        $("#tweet-text").val("");
+        $(this).find('.counter').val('140');
+    
+      });
+    
+      loadTweets();
+    })
